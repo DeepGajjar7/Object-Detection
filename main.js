48ba0f172b0f197a544eeb8ac3b3ad1a77182270
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-06-29 20:45:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var img="";
+var model_status="";
 
 function preload(){
     img=loadImage("dog_cat.jpg");
@@ -7,6 +8,9 @@ function preload(){
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
+
+    objectdetector=ml5.objectDetector('cocossd',modeloaded)
+    document.getElementById("status").innerHTML="status: detecting objects";
 }
 
 function draw(){
@@ -16,4 +20,39 @@ function draw(){
     noFill();
     stroke("red");
     rect(30,60,450,350);
+
+    fill("red");
+    text("cat",320,120);
+    noFill();
+    stroke("red");
+    rect(300,90,270,320);
 }
+
+function modeloaded(){
+    console.log("model is loaded");
+    model_status="true";
+    objectdetector.detect(img,gotresults)
+}
+
+function gotresults(error,results){
+    if(error){
+        console.log(error)
+    }
+
+    else{
+        console.log(results)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
