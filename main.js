ffdd725e45eb225ca31213ef2c172c68c8feb307
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-07-04 20:35:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var img="";
 var model_status="";
+var objects=[];
 
 function preload(){
     img=loadImage("dog_cat.jpg");
@@ -15,17 +16,17 @@ function setup(){
 
 function draw(){
     image(img,0,0,640,420);
-    fill("red");
-    text("dog",45,75);
-    noFill();
-    stroke("red");
-    rect(30,60,450,350);
-
-    fill("red");
-    text("cat",320,120);
-    noFill();
-    stroke("red");
-    rect(300,90,270,320);
+    if (model_status !=""){
+        for (i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="Status: objects detected";
+        fill("#FF0000");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15)
+            noFill();
+            stroke("#FF0000")
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            }
+    }
 }
 
 function modeloaded(){
@@ -41,6 +42,7 @@ function gotresults(error,results){
 
     else{
         console.log(results)
+        objects=results;
     }
 }
 
